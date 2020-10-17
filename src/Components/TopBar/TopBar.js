bc61fba80e9fcca956a6a5fe26dbd287cfe0e263
Anmol Tanwer <63873607+anmoltanwer@users.noreply.github.com>
2020-10-17 17:42:45 +0530

    Update TopBar.js

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -13,9 +13,27 @@ const TopBar = () => {
                         <p>View Jobs</p>
                     </nav>
                 </div>
+                <div className = {classes.dropdown}>
+                    <img className  ={classes.MenuBar} src ="https://assessments.edyoda.com/static/images/burger-svg-icon.svg" alt="menubar"/>
+                   
+                </div>
                 <div className = {classes.infoContainer}>
-                    <i class="fas fa-bell"></i>
-                    <p>Anmol Tanwer</p>  
+                   <i class="fa fa-bell-o fa-lg">
+    
+                        <span className ={classes.notifyNum}>5</span>
+                    </i>
+                    <div className = {classes.dropdown}>
+                    <p className = {classes.AdminName}>Anmol Tanwer</p> 
+                        <div className = {classes.AdminDropDown}>
+                        <p>Profile</p>
+                        <p>Update Job Profile</p>
+                        <hr/>
+                        <p>Logout</p>
+                        <p>Change Password</p>   
+                    </div>
+                    </div> 
+                   <i class="fas fa-caret-down"></i>
+                     
                 </div>
             </div>
         </>
